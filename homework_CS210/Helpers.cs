155f6a764f211c82bc733522d068fd3f7d952d55
lisa <yelyzaveta.ushkevych.25@kse.org.ua>
2026-03-15 13:37:31 +0200

    added operators

---
+++ homework_CS210/Helpers.cs
@@ -0,0 +1,54 @@
+namespace homework_CS210;
+
+public class Helpers
+{
+    public static bool IsOperator(string s)
+    {
+        if (s == "+")
+            return true;
+        if (s == "-")
+            return true;
+        if (s == "*")
+            return true;
+        if (s == "/")
+            return true;
+
+        return false;
+        
+    }
+
+    public static int Priority(string op)
+    {
+        if (op == "+" || op == "-")
+            return 1;
+        if (op == "*" || op == "/")
+            return 2;
+
+        return 0;
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
